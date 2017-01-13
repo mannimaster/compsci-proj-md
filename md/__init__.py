b6a503ab5ef0d8e007e70d4409239cfc4143b505
harmening <nils.harmening@fu-berlin.de>
2017-01-13 08:28:52 +0100

    add contributors

--- md/__init__.py
+++ md/__init__.py
@@ -1,5 +1,6 @@
 #   md - Molecular Dynamics Applied to ionic solids.
-#   Copyright (C) 2017 Nils Harmening
+#   Copyright (C) 2017 Nils Harmening, Marco Manni,
+#   Darian Steven Viezzer, Steffi, Hendrik
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
