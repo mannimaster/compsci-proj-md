3db315f8787c020486e80dafb2e8b317025cb3f6
mannimaster <marco_manni@hotmail.com>
2017-01-22 17:02:08 +0100

    Added Maxwell_Boltzmann Distribution sampler

--- md/distribution.py
+++ md/distribution.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 from abc import ABCMeta, abstractmethod, abstractproperty
 
 
@@ -21,12 +21,40 @@ class maxwellboltzmann(__distribution):
         #probably implementable, otherwise a NotImplementedError as well ?
         return
 
-
-    def generate_distribution(self):
-        # do anything
-        return None
+    def sample_distribution(self,N,m,T,kB = 1.38064852e-23):
+         ''' Sample velocities from the Maxwell-Boltzmann Distribution for a given chemical species
 
 
-    def sample_distribution(self):
-        # do anything
-        return None
+        Parameters
+        ------------
+        N: int
+            Number of Particles
+
+        m: Nx1 Array, or float
+            mass of Particles, if m is a float, then all particles are assumed to have the same mass. 
+
+        T: float
+            Temperature in Kelvin
+
+        kB: float
+            Boltzmann Constant  = 1.38064852e-23
+
+        Returns
+        -----------
+        v: N dimensional Array
+            contains the velocities of each particle
+        '''
+        sigma = kB*T/m
+        Velocities = np.zeros((N,3))
+
+        v_x = np.sqrt(sigma)*np.random.normal(size=N)
+        v_y = np.sqrt(sigma)*np.random.normal(size=N)
+        v_z = np.sqrt(sigma)*np.random.normal(size=N)
+
+    #     v_total = np.sqrt(v_x**2+v_y**2+v_z**2)
+
+        Velocities[:,0]= v_x
+        Velocities[:,1]= v_y
+        Velocities[:,2]= v_z
+
+        return Velocities
