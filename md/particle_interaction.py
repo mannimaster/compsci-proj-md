c2cefe7665fef68a60e4406cdbecf1a459473202
mannimaster <marco_manni@hotmail.com>
2017-01-23 20:51:20 +0100

    Added Short range force

--- md/particle_interaction.py
+++ md/particle_interaction.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
+from boxvectors import directions as directions
 
 '''
 Abstract class for implementing particle interaction
@@ -59,10 +60,72 @@ class coloumb(__particle_interaction):
         return None
 
 
-    def __short_range_forces(self):
-        # do anything
-        # use from super the result of neighbourlist
-        return None
+    def __short_range_forces(self,Positions,R,Sigma, Epsilon, Labels, std, L, Directions):
+        ''' Calculate the Force resulting from the short range coulomb interaction between the Particles
+
+        Parameters
+        ---------------
+
+        Positions: Nx3 Array
+            Array with N rows and 3 Columns, each row i contains the x,y and z coordinates of particle i.
+
+        R: Nx1 Array
+            Array with N entries. Contains each Particles Distance to the coordinate origin.
+
+        Sigma: 3x1 Array
+            Array with 3 entries. Regarding 2 Particles the entries are: Sigma = [sigma_AA, simga_AB, sigma_BB].
+
+        Epsilon: 3x1 Array
+            Array with 3 entries. Regarding 2 Particles the entries are: Epsilon = [epsilon_AA, epsilon_AB, epsilon_BB].
+
+        Labels: Nx? Array
+            Array with N rows and ? Columns. The third Column should contain labels, that specify the chemical species of the Particles.
+            Particle A should have the label 1 and Particle B should have the label 0.
+
+        std: float
+            Sigma that is mentioned in the coulomb forces.
+
+        L:3x1 Array
+            Array containg the Dimensions of the Simulation box
+
+        Directions: Array
+            Array that contains Pointers to neighbouring boxes
+
+        Returns
+        --------------
+
+        Force_short_range: Nx3 Array
+            Array with N rows and 3 Columns. Each Row i contains the short-range-Force acting upon Particle i componentwise. 
+
+        '''
+        K = Directions
+        K[:,0] *=L[0]
+        K[:,1] *=L[1]
+        K[:,2] *=L[2]
+
+        N = np.size(Positions[:,0])
+        Force_short_range = np.zeros((N,3))
+        k = np.size(K[:,0])
+
+
+        for i in np.arange(N):
+
+            dists_single_cell = Positions[i,:]-Positions
+            charges = np.resize( Labels[current_Neighbors,1] , np.shape(dists_single_cell))
+
+            #This Skript paralellizes the sum over j and executes the sum over vectors n within the loop
+            for j in np.arange(k):
+                dists = dists_single_cell+K[j]
+
+                norm_dists = np.linalg.norm(dists)
+
+                Force_short_range[i,:] += np.sum(charges*dists/norm_dists**2 
+                *( erfc( dists/np.sqrt(2)/std )/norm_dists 
+                + np.sqrt(2.0/np.pi)/std*np.exp(-norm_dists**2/(2*std**2) )) ,0)
+
+        #Getting the Pre-factor right        
+        Force_short_range = Force_short_range* np.resize( Labels[:,1], np.shape(Positions) ) /(8*np.pi*Epsilon_0)
+        return Force_short_range
 
 
     def __long_range_forces(self):
@@ -133,9 +196,13 @@ class lennard_jones(__particle_interaction):
             eps = (Epsilon*np.ones( (N,3) ) )[0,index]
 
             Positions_Difference = Positions[i,:] - Positions
+            
+            Positions_Difference[:,0] = Positions_Difference[:,0]%(L[0]/2)
+            Positions_Difference[:,1] = Positions_Difference[:,0]%(L[1]/2)
+            Positions_Difference[:,2] = Positions_Difference[:,0]%(L[2]/2)
             for j in Neighbors[i]:
                 #Calculate the Distances 
-                dist = np.abs(R[i]-R[j])
+                dist = np.linalg.norm(Positions_Difference[j,:])
 
                 #Calculate the Value of the switch Function
                 Switch = (switch_parameter[0]+switch_parameter[1]*dist+switch_parameter[2]*dist**2+switch_parameter[3]*dist**3)
@@ -147,7 +214,7 @@ class lennard_jones(__particle_interaction):
 
                 if dist < r_switch:
                     #This is the Analytical Expression for the LJ force
-                    Force_LJ[i,:] += 48.0 *eps[j] *sig6[j] *Positions_Difference[i,:] /dist**8 *(-sig6_dist_ratio +0.5)
+                    Force_LJ[i,:] += 48.0 *eps[j] *sig6[j] *Positions_Difference[j,:] /dist**8 *(-sig6_dist_ratio +0.5)
                 else:
                     #This is the derivative of the switch-function, used to cut the LJ-Potential
                     Force_LJ[i,:] += 8.0 *eps[j] *Positions_Difference[i,:] *sig6_dist_ratio
