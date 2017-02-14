6a4d23931eeadda557d3648db429e82ea1062b1b
daviezz <darian.viezzer@fu-berlin.de>
2017-02-14 21:41:32 +0100

    first md object test

--- md/test_api.py
+++ md/test_api.py
@@ -17,69 +17,76 @@
 
 import numpy as np
 
-#from .api import md
+# from .api import md
 from particle_interaction import coulomb
 from particle_interaction import lennard_jones
-from neighbourlist import neighbourlist 
+from neighbourlist import neighbourlist
 import Initial_Test_Parameters as ip
 from md import System
+from md import md
+from distribution import maxwellboltzmann
 
-Symbols = ["Na" , "Cl"]
+Symbols = ["Na", "Cl"]
 Coefficients = [1, 1]
 Charges = [+1.0, -1.0]
-N = 1*np.sum(Coefficients)
-#k_cut = ip.k_cut
-#k_max = ip.k_max_long_range
+N = 1 * np.sum(Coefficients)
+# k_cut = ip.k_cut
+# k_max = ip.k_max_long_range
 n_boxes_short_range = ip.n_boxes_short_range
 n_boxes_LJ = ip.n_boxes_LJ
 r_cut_LJ = ip.r_cut_LJ
-r_switch = r_cut_LJ*0.9
-Sys= System(Symbols, Coefficients, Charges, N/2)
+r_switch = r_cut_LJ * 0.9
+Sys = System(Symbols, Coefficients, Charges, N / 2)
 Sigma, Epsilon = Sys.get_LJ_parameter()
 p_error = ip.p
 switch_parameter = ip.switch_parameter
-Test_Positions = np.array([[1,1,1],
-                           [2,2,2]])
+Test_Positions = np.array([[1, 1, 1],
+                           [2, 2, 2]])
 Test_R = np.linalg.norm(Test_Positions)
 Test_L = ip.L
-Test_Labels = np.array([[1,+1.0,0],
-                        [1,-1.0,1]])
+Test_Labels = np.array([[1, +1.0, 0],
+                        [1, -1.0, 1]])
 
-Test_d_Pos = np.zeros((N,N,3))
-Test_d_Pos[:,:,0] = np.subtract.outer(Test_Positions[:,0], Test_Positions[:,0])
-Test_d_Pos[:,:,1] = np.subtract.outer(Test_Positions[:,1], Test_Positions[:,1])
-Test_d_Pos[:,:,2] = np.subtract.outer(Test_Positions[:,2], Test_Positions[:,2])
+Test_d_Pos = np.zeros((N, N, 3))
+Test_d_Pos[:, :, 0] = np.subtract.outer(Test_Positions[:, 0], Test_Positions[:, 0])
+Test_d_Pos[:, :, 1] = np.subtract.outer(Test_Positions[:, 1], Test_Positions[:, 1])
+Test_d_Pos[:, :, 2] = np.subtract.outer(Test_Positions[:, 2], Test_Positions[:, 2])
 
 neighbours = neighbourlist().compute_neighbourlist(Test_Positions, Test_L[0], r_cut_LJ)[0]
-  
-  
+
+Test_dt = ip.dt
+
+
 def test_get_dircetions():
     from boxvectors import directions
-    #Create Test Array
+    # Create Test Array
     d = directions(3)
     K = d.get_directions()
-    #Shift all entries up, to avoid negative entries
+    # Shift all entries up, to avoid negative entries
     n_boxes = 3
     K += n_boxes
-    #define Base
-    base = (2*n_boxes+1)
+    # define Base
+    base = (2 * n_boxes + 1)
 
-    #Make a test Array
-    K_test = np.zeros(base**3)
-    K_test = K[:,0]*base**2 +K[:,1]*base +K[:,2]
-    assert np.size(np.unique(K_test)) == base**3, "get_dircetions is broken"
+    # Make a test Array
+    K_test = np.zeros(base ** 3)
+    K_test = K[:, 0] * base ** 2 + K[:, 1] * base + K[:, 2]
+    assert np.size(np.unique(K_test)) == base ** 3, "get_dircetions is broken"
     return "Passed"
+
 
 def test_coulomb_forces():
     c = coulomb(n_boxes_short_range,
                 Test_L,
                 p_error)
-    c.compute_optimal_cutoff(p_error, Test_L, Test_Labels, neighbours[0], neighbours[1], r_switch, 0.49 * Test_L[0], Test_Positions)
+    c.compute_optimal_cutoff(p_error, Test_L, Test_Labels, neighbours[0], neighbours[1], r_switch, 0.49 * Test_L[0],
+                             Test_Positions)
     Force = c.compute_forces(Test_d_Pos,
                              Test_Labels,
                              Test_L)
-    assert np.all(Force[0,:] == -Force[1,:]), "coulomb force is broken"
-    
+    assert np.all(Force[0, :] == -Force[1, :]), "coulomb force is broken"
+
+
 def test_LJ_forces():
     LJ = lennard_jones()
     Force = LJ.compute_forces(Test_Positions,
@@ -90,14 +97,15 @@ def test_LJ_forces():
                               switch_parameter,
                               r_switch,
                               neighbours)
-    assert np.all(Force[0,:] == -Force[1,:]), "lennard Jones force is broken"
+    assert np.all(Force[0, :] == -Force[1, :]), "lennard Jones force is broken"
+
 
 def test_neighborlist():
     N = 100
-    R=np.random.rand(N,3)
-    box_length=1.0
-    r_cutoff=0.11
-   
+    R = np.random.rand(N, 3)
+    box_length = 1.0
+    r_cutoff = 0.11
+
     naiveneighbors = {}
     dx = np.empty(3)
     for i in range(N):
@@ -105,69 +113,95 @@ def test_neighborlist():
         for j in range(N):
             d = 0.0
             for x in range(3):
-                dx[x] = np.abs(R[i][x]-R[j][x])
-                if (dx[x] < -box_length/2):
+                dx[x] = np.abs(R[i][x] - R[j][x])
+                if (dx[x] < -box_length / 2):
                     dx[x] += box_length
-                elif (dx[x] > box_length/2):
+                elif (dx[x] > box_length / 2):
                     dx[x] -= box_length
 
-                d += dx[x]**2
-            
+                d += dx[x] ** 2
+
             d = np.sqrt(d)
             if (d <= r_cutoff):
-                if i>j:
+                if i > j:
                     naiveneighbors[i].append(j)
                     naiveneighbors[j].append(i)
 
-   
     from neighbourlist import neighbourlist as nbl
     n2 = naiveneighbors
     n_inst = nbl()
     n1, dist = n_inst.compute_neighbourlist(R, box_length, r_cutoff)
     for i in range(N):
-      n1[i].sort()
-      n2[i].sort()
+        n1[i].sort()
+        n2[i].sort()
 
     assert n1 == n2
 
 
 def test_SymmetriesPotC():
-    #tests coulomb potential function with equidistant charges where the middle one has twice the negativ charge
-    potential        = coulomb(ip.n_boxes_short_range,ip.L, ip.p)
-    potential.compute_optimal_cutoff(p_error, Test_L, Test_Labels, neighbours[0], neighbours[1], r_switch, 0.49 * Test_L[0], Test_Positions)
-    result           = potential.compute_potential(positions=ip.positions, labels=ip.labels, neighbours=ip.neighbours,
-                                                   distances=ip.distances, r_s=np.sqrt(3*2**2), r_c=np.sqrt(3*2**2)+0.1)
-    assert ( abs(result[0]/result[2])<1+10**(-8) ) , "Potential does not have the expected symmetrie. P1 and P3 should be the same."
-    assert ( abs(result[0]/result[1])<0.5+10**(-8) ) , "Potential does not have the expected symmetrie. P2 should be P1*2."
-    assert ( abs(result[2]/result[1])<0.5+10**(-8) ) , "Potential does not have the expected symmetrie. P2 should be P3*2."
+    # tests coulomb potential function with equidistant charges where the middle one has twice the negativ charge
+    potential = coulomb(ip.n_boxes_short_range, ip.L, ip.p)
+    potential.compute_optimal_cutoff(p_error, Test_L, Test_Labels, neighbours[0], neighbours[1], r_switch,
+                                     0.49 * Test_L[0], Test_Positions)
+    result = potential.compute_potential(positions=ip.positions, labels=ip.labels, neighbours=ip.neighbours,
+                                         distances=ip.distances, r_s=np.sqrt(3 * 2 ** 2), r_c=np.sqrt(3 * 2 ** 2) + 0.1)
+    assert (abs(result[0] / result[2]) < 1 + 10 ** (
+    -8)), "Potential does not have the expected symmetrie. P1 and P3 should be the same."
+    assert (
+    abs(result[0] / result[1]) < 0.5 + 10 ** (-8)), "Potential does not have the expected symmetrie. P2 should be P1*2."
+    assert (
+    abs(result[2] / result[1]) < 0.5 + 10 ** (-8)), "Potential does not have the expected symmetrie. P2 should be P3*2."
     return
 
 
 def test_SymmetriesPotLJ():
-    #tests LJ Potential for  with equidistant identical charges
+    # tests LJ Potential for  with equidistant identical charges
 
     potential = lennard_jones()
-    result    = potential.compute_potential(sigma=ip.sigma, epsilon=ip.epsilon, labels=ip.labels,
-                                            distances=ip.distances, neighbours=ip.neighbours, r_s=np.sqrt(3*2**2), r_c=np.sqrt(3*2**2)+0.1)
-    assert ( abs(result[0]/result[2])<1+10**(-8) ) , "Potential does not have the expected symmetrie. P1 and P3 should be the same."
-    assert ( abs(result[0]/result[1])<1+10**(-8) ) , "Potential does not have the expected symmetrie. P1 and P2 should be the same."
-    assert ( abs(result[2]/result[1])<1+10**(-8) ) , "Potential does not have the expected symmetrie. P3 and P2 should be the same."
+    result = potential.compute_potential(sigma=ip.sigma, epsilon=ip.epsilon, labels=ip.labels,
+                                         distances=ip.distances, neighbours=ip.neighbours, r_s=np.sqrt(3 * 2 ** 2),
+                                         r_c=np.sqrt(3 * 2 ** 2) + 0.1)
+    assert (abs(result[0] / result[2]) < 1 + 10 ** (
+    -8)), "Potential does not have the expected symmetrie. P1 and P3 should be the same."
+    assert (abs(result[0] / result[1]) < 1 + 10 ** (
+    -8)), "Potential does not have the expected symmetrie. P1 and P2 should be the same."
+    assert (abs(result[2] / result[1]) < 1 + 10 ** (
+    -8)), "Potential does not have the expected symmetrie. P3 and P2 should be the same."
     return
 
 
 def test_SymmetriesPotLJ2():
-
     potential = lennard_jones()
-    result    = potential.compute_potential(sigma=ip.sigma, epsilon=ip.epsilon, labels=ip.labels,
-                                            distances={0: [np.sqrt(12), np.sqrt(3)], 1: [np.sqrt(12), np.sqrt(12)], 2: [np.sqrt(3), np.sqrt(12)]},
-                                            neighbours=ip.neighbours, r_s=np.sqrt(3*2**2), r_c=np.sqrt(3*2**2)+0.1)
-    assert ( abs(result[0]/result[2])<1+10**(-8) ) , "Potential does not have the expected symmetrie. P1 and P3 should be the same."
-    assert ( result[0]!=result[1] )                , "Potential does not have the expected symmetrie. P1 and P2 should not be the same."
-    assert ( result[2]!=result[1] )                , "Potential does not have the expected symmetrie. P3 and P2 should not be the same."
+    result = potential.compute_potential(sigma=ip.sigma, epsilon=ip.epsilon, labels=ip.labels,
+                                         distances={0: [np.sqrt(12), np.sqrt(3)], 1: [np.sqrt(12), np.sqrt(12)],
+                                                    2: [np.sqrt(3), np.sqrt(12)]},
+                                         neighbours=ip.neighbours, r_s=np.sqrt(3 * 2 ** 2),
+                                         r_c=np.sqrt(3 * 2 ** 2) + 0.1)
+    assert (abs(result[0] / result[2]) < 1 + 10 ** (
+    -8)), "Potential does not have the expected symmetrie. P1 and P3 should be the same."
+    assert (result[0] != result[1]), "Potential does not have the expected symmetrie. P1 and P2 should not be the same."
+    assert (result[2] != result[1]), "Potential does not have the expected symmetrie. P3 and P2 should not be the same."
     return
 
-    
 
-test_SymmetriesPotC()
-test_SymmetriesPotLJ()
-test_SymmetriesPotLJ2()
+def test_get_energy():
+    N = Test_Positions.shape[1]
+    Positions = np.zeros((N, 3))
+    Positions[:, 0] = np.linspace(0.1, L[0], N, endpoint=False)
+    Positions[:, 1] = np.linspace(0.1, L[1], N, endpoint=False)
+    Positions[:, 2] = np.linspace(0.1, L[2], N, endpoint=False)
+    np.random.shuffle(Positions[:, 0])
+    np.random.shuffle(Positions[:, 1])
+    np.random.shuffle(Positions[:, 2])
+    Velocities = maxwellboltzmann().sample_distribution(N, Test_Labels[:, 0], ip.T)
+    Forces = np.zeros(Velocities.shape)
+
+    try:
+        MDobj = md.md(positions=Positions, properties=Test_Labels, velocities=Velocities, forces=Forces, box=Test_L,
+                  Temperature=ip.T, Sigma_LJ=ip.sigma, Epsilon_LJ=ip.epsilon, r_switch=r_switch, r_cut_LJ=ip.r_cut_LJ,
+                  n_boxes_short_range=n_boxes_short_range, dt=ip.dt, p_rea=10 ** -2, p_error=ip.p, Symbols=Symbols)
+    except:
+        MDobj=0
+
+    assert(isinstance(MDobj,md))
+    return
